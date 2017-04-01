3f4dfa7a59cef5eb39cb4fc8d1fb6e8783df137f
Bill Boughton <boughton.william@gmail.com>
2017-04-01 08:34:43 -0600

    all routes working in 24 minutes

--- routes/nfl.js
+++ routes/nfl.js
@@ -8,6 +8,42 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+  var getId = req.params.id
+  knex('nfl').where('id', getId)
+    .then(result => {
+      res.send(result)
+    })
+})
 
+router.post('/new', (req, res) => {
+  var newConf = req.body.conf
+  var newSb = req.body.sb
+  var newTeam = req.body.team
+  knex('nfl').insert({conf:newConf, sb:newSb, team:newTeam}).returning(['id', 'team', 'conf', 'sb'])
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  var patchId = req.params.id
+  var changeConf = req.body.conf
+  var changeSb = req.body.sb
+  var changeTeam = req.body.team
+  knex('nfl').where('id', patchId).update({conf:changeConf, sb:changeSb, team:changeTeam}).returning(['id', 'team', 'conf', 'sb'])
+    .then(result => {
+      res.send(result)
+    })
+})
+
+
+router.delete('/:id', (req, res) => {
+  var deleteId = req.params.id
+  knex('nfl').where('id', deleteId).del()
+    .then(result => {
+      res.send(200)
+    })
+})
 
 module.exports = router
